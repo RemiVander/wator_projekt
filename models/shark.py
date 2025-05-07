847c5a01b1a52afd3fa3d581a308069e9bee7957
RemiVander <rm.vanderperre@gmail.com>
2025-05-07 14:15:36 +0200

    spawn sharks

--- models/shark.py
+++ models/shark.py
@@ -4,3 +4,7 @@ class Shark(Fish):
     def __init__(self,reproduce, x_coordinate, y_coordinate):
         super().__init__(reproduce, x_coordinate, y_coordinate)
         self.energy = 5
+
+    
+    def spawn(self, x, y):
+        return Shark(x, y, self.reproduce, self.energy)
